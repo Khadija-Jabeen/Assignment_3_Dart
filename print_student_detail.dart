aebbcd0a74fc80093c685c3ae579670193af632e
Khadija-Jabeen <khadijajabeen2004@gmail.com>
2024-09-07 10:44:39 -0700

    question-3

---
+++ print_student_detail.dart
@@ -0,0 +1,6 @@
+void main() {
+  List<String> names = ['Alice', 'Bob', 'Charlie', 'David'];
+  for (String name in names) {
+    print(name);
+  }
+}
---
+++ student_detail.dart
@@ -0,0 +1,4 @@
+void main() {
+  List<dynamic> student = ['Alice', 'Class 10', 21, 'A', 89.5];
+  print(student);
+}
