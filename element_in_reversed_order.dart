ee64096706e3613d598790b5dcda19f3060e77f1
Khadija-Jabeen <khadijajabeen2004@gmail.com>
2024-09-07 11:12:10 -0700

    question-6

---
+++ element_in_reversed_order.dart
@@ -0,0 +1,7 @@
+void main() {
+  List<String> names = ['Alice', 'Bob', 'Charlie', 'David'];
+  List<String> reversedNames = List.from(names.reversed);
+
+  print('Original list: $names');
+  print('Reversed list: $reversedNames');
+}
